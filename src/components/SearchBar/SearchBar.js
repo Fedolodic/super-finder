28e4e6df499f91408551ec931af26ebfae27d1e1
David Martinez <david.mtz00@hotmail.com>
2020-05-22 21:51:14 -0500

    Render the <SearchBar /> Component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -13,8 +13,28 @@ class SearchBar extends React.Component {
     // This method will be used when building the structure of the search bar component
     renderSortByOptions() {
         return Object.keys(sortByOptions).map(sortByOption => {
-           let sortByOptionValue = sortByOptions[sortByOption];
-           return <li key={sortByOptionValue}>{sortByOption}</li>
+            let sortByOptionValue = sortByOptions[sortByOption];
+            return <li key={sortByOptionValue}>{sortByOption}</li>
         });
     };
+
+    render() {
+        return (
+            <div className="SearchBar">
+                <div className="SearchBar-sort-options">
+                    <ul>
+                        <!-- Use .renderSortByOptions() to sort the businesses by their options -->
+                        {this.renderSortByOptions()}
+                    </ul>
+                </div>
+                <div className="SearchBar-fields">
+                    <input placeholder="Search Businesses"/>
+                    <input placeholder="Where?"/>
+                </div>
+                <div className="SearchBar-submit">
+                    <a>Let's Go</a>
+                </div>
+            </div>
+        )
+    }
 }
