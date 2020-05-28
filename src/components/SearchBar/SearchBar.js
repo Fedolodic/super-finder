318911e5ce1a466d27f215d89f44e8a74a95efee
David Martinez <david.mtz00@hotmail.com>
2020-05-27 19:52:38 -0500

    Get a Sort Option's Class

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,32 +1,38 @@
 import React from 'react';
 import './SearchBar.css';
 
-const sortByOptions = {
-    'Best Match': 'best_match',
-    'Highest Rated': 'rating',
-    'Most Reviewed': 'review_count'
-};
-
 class SearchBar extends React.Component {
     constructor(props) {
         super(props);
-
         /*term => search term located in the search input
         * location => location to search near from the location input
         * sortBy => selected sorting option to use*/
         this.state = {
             term: '',
             location: '',
-            sortBt: 'best_match'
+            sortBy: 'best_match'
         };
+        this.sortByOptions = {
+            'Best Match': 'best_match',
+            'Highest Rated': 'rating',
+            'Most Reviewed': 'review_count'
+        };
+    }
+
+    getSortByClass(sortByOption) {
+        if (this.state.sortBy === sortByOption) {
+            return 'active';
+        } else {
+            return '';
+        }
     }
 
     /*Purpose is to dynamically create the list items needed to display the sort options (Best Match, Highest Rated,
     Most Reviewed). This is to help future proof against potential changes to the Yelp API.
     This method will be used when building the structure of the search bar component*/
     renderSortByOptions() {
-        return Object.keys(sortByOptions).map(sortByOption => {
-            let sortByOptionValue = sortByOptions[sortByOption];
+        return Object.keys(this.sortByOptions).map(sortByOption => {
+            let sortByOptionValue = this.sortByOptions[sortByOption];
             return <li key={sortByOptionValue}>{sortByOption}</li>
         });
     };
