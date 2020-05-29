5d5b1ec208e57f4247dd93a9430124be94cd83fb
David Martinez <david.mtz00@hotmail.com>
2020-05-28 22:16:38 -0500

    Add a key to <Business/>

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -6,7 +6,7 @@ import Business from '../Business/Business';
 class BusinessList extends React.Component {
     render() {
         return (
-            <div className="BusinessList">
+            <div className="BusinessList" key={business.id}>
                 {/*Iterate through businesses array from parent component,
                 and return Business components*/}
                 {this.props.businesses.map(business => <Business business={business}/>)}
