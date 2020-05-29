be365f1bec18da2acc78cca4a32903e854a6a7c1
David Martinez <david.mtz00@hotmail.com>
2020-05-28 21:15:19 -0500

    Add Search()

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,5 +1,24 @@
 const apiKey = 'huPlqIKAyngHBkBTwYGMC_gF0zWhnXuO1dfZ-ircpjaJKUGVCJUuDhhhnzvO66KVJSAWFJ2aEiMCPHVcE8nlJL3C2CC0Jyda8TlQKYn_iMJMJKkZQdP7CqpCQDvQXnYx';
 
 yelp = {
-
+    search(term, location, sortBy) {
+        /*fetch() won't function correctly due to CORS restrictions. We can bypass this restriction
+          with an API called CORS Anywhere. CORS Anywhere will take requests sent to its API endpoint,
+          make them for the requesting app with the proper CORS permissions, and then return
+          the response back to the requesting app*/
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+            headers: {
+                Authorization: `Bearer ${apiKey}`
+            }
+        }).then(
+            response => response.json()
+        ).then(
+            jsonResponse => {
+                /*If valid response returned from the Yelp API*/
+                if (jsonResponse.businesses) {
+                    return jsonResponse.businesses.map(business => {});
+                }
+            }
+        );
+    }
 };
