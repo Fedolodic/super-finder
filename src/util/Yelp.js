eac06e84dcabdfb943ebdd721a9b6bca0e599357
David Martinez <david.mtz00@hotmail.com>
2020-05-28 22:11:04 -0500

    Access the JSON Response

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,6 +1,8 @@
+import React from 'react';
+
 const apiKey = 'huPlqIKAyngHBkBTwYGMC_gF0zWhnXuO1dfZ-ircpjaJKUGVCJUuDhhhnzvO66KVJSAWFJ2aEiMCPHVcE8nlJL3C2CC0Jyda8TlQKYn_iMJMJKkZQdP7CqpCQDvQXnYx';
 
-yelp = {
+export default class Yelp extends React.Component {
     search(term, location, sortBy) {
         /*fetch() won't function correctly due to CORS restrictions. We can bypass this restriction
           with an API called CORS Anywhere. CORS Anywhere will take requests sent to its API endpoint,
@@ -14,9 +16,23 @@ yelp = {
             response => response.json()
         ).then(
             jsonResponse => {
+                console.log(jsonResponse);
                 /*If valid response returned from the Yelp API*/
                 if (jsonResponse.businesses) {
-                    return jsonResponse.businesses.map(business => {});
+                    return jsonResponse.businesses.map(business => {
+                        return {
+                            id: business.id,
+                            imageSrc: business.image_url,
+                            name: business.name,
+                            address: business.address1,
+                            city: business.location.city,
+                            state: business.location.state,
+                            zipCode: business.location.zip_code,
+                            category: business.categories.title,
+                            rating: business.rating,
+                            reviewCount: business.review_count
+                        }
+                    });
                 }
             }
         );
